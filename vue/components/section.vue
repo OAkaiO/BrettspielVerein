fc7d2e5def721498f809dad5d9d4a60e2327b7c2
Lukas Schär <luki.schaer@hispeed.ch>
2025-02-26 18:17:27 +0100

    004: Add generic section component with header

---
+++ vue/components/section.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title: string;
+}>();
+</script>
+
+<template>
+  <div>
+      <v-container>
+        <h1>{{ title }}</h1>
+        <slot></slot>
+      </v-container>
+  </div>
+</template>
+
+<style>
+h1 {
+  text-align: center;
+}
+</style>
