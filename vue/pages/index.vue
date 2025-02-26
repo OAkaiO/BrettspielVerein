9e00bf9a6a414ac4e4f3791ce3958671f18be5aa
Lukas Schär <luki.schaer@hispeed.ch>
2025-02-26 18:17:39 +0100

    004: Add examples of the new components

--- vue/pages/index.vue
+++ vue/pages/index.vue
@@ -1,8 +1,54 @@
 <template>
-  <div>
+  <p>
+    <NuxtLink to="tryhard">Try this!</NuxtLink>
+  </p>
+  <ImageContainer :variant="1">
     <p>
-      <NuxtLink to="tryhard">Try this!</NuxtLink>
+      ptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita
+      kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+      voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+      clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+      amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+      nonu ptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+      clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+      amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+      nonumy eirmod tempor invidunt ut labore et
     </p>
+  </ImageContainer>
+
+  <Section :title="'Über uns'">
+    <div>
+      Wir treffen uns 1x im Monat jeweils an einem Freitagabend im Spittelhof
+      Zofingen und spielen Brettspiele jeder Art. Von gehobenen Familienspielen,
+      bis mehrstündigen Expertenspielen ist für Jeden etwas dabei!
+      Spielerfahrung ist von Vorteil, aber absolut nicht erforderlich. Die
+      Teilnahme an unserem Spieleabend steht allen Altersgruppen offen. Bei uns
+      werden einige Spiele zur Verfügung gestellt und erklärt. Es dürfen aber
+      sehr gerne auch eigene Spiele mitgebracht werden! Perfekt also für alle,
+      die zu Hause wegen fehlenden Mitspielern noch ungespielte Spiele rumliegen
+      haben, die schon lange wieder mal ein bestimmtes Spiel spielen wollten und
+      natürlich auch für alle anderen, die einfach gerne Spiele spielen.
+    </div>
+  </Section>
+
+  <ImageContainer :variant="2">
+    <p>
+      ptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita
+      kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+      voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+      clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+      amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+      nonu ptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+      clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+      amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+      nonumy eirmod tempor invidunt ut labore et
+    </p>
+  </ImageContainer>
+  <v-container>
     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
     eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
     voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
@@ -250,5 +296,5 @@
     amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
     nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
     diam voluptua.
-  </div>
+  </v-container>
 </template>
