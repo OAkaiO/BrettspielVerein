21bd537d7e6aece85870561cbba2f8b3fb014daf
Lukas Schär <luki.schaer@hispeed.ch>
2025-03-09 19:04:09 +0100

    004: Add basic event list

---
+++ vue/components/event/details.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+
+const props = defineProps<{data: EventData}>()
+</script>
+
+<template>
+    <VContainer>
+        <table>
+            <tbody>
+                <tr>
+                    <td>Ort:</td>
+                    <td>{{ data.location }}</td>
+                </tr>
+                <tr>
+                    <td>Zeit:</td>
+                    <td>{{ data.time }}</td>
+                </tr>
+                <tr>
+                    <td>Eintritt:</td>
+                    <td>{{ data.price }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </VContainer>
+</template>
+
+<style scoped>
+tr td:nth-child(1) {
+    font-weight: bold;
+}
+
+tr td:nth-child(2) {
+    padding-left: 12px;
+}
+</style>
