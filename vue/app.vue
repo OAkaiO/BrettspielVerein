f234b53d4b7913bc04368d27e9c41bbdea3ad3c2
Lukas Schär <luki.schaer@hispeed.ch>
2025-03-12 09:08:53 +0100

    004: Add a toggleable navbar for small screens

--- vue/app.vue
+++ vue/app.vue
@@ -2,7 +2,9 @@
 const goTo = useOffsetGoTo();
 
 const page = ref();
-const sections : ComputedRef<Array<HeaderSpec>> = computed(() => page.value?.pageRef.sections);
+const sections: ComputedRef<Array<HeaderSpec>> = computed(
+  () => page.value?.pageRef.sections
+);
 
 const { arrivedState } = useWindowScroll();
 const scrolledOverState = computed(() => {
@@ -12,6 +14,9 @@ const scrolledOverState = computed(() => {
       (index == Object.keys(sections.value).length - 1 && arrivedState.bottom)
   );
 });
+
+const drawerVisibility = ref(false);
+const { mdAndUp } = useDisplay();
 </script>
 
 <template>
@@ -19,26 +24,48 @@ const scrolledOverState = computed(() => {
     <VAppBar :elevation="0" color="header">
       <VContainer>
         <VRow class="align-center justify-space-between px-4">
-            <LogoTextWrapper onPrimary></LogoTextWrapper>
+          <LogoTextWrapper onPrimary></LogoTextWrapper>
           <div class="nav-container" v-if="!!sections">
-            <div
-              class="hover-link scrolled-over d-inline mx-2"
-              @click="goTo(0)"
-            >
-              Home
-            </div>
-            <div
-              v-for="(sec, index) in sections"
-              class="hover-link d-inline mx-2"
-              @click="goTo(sec.ref)"
-              :class="{ 'scrolled-over': scrolledOverState[index] }"
-            >
-              {{ sec?.displayName }}
-            </div>
+            <template v-if="mdAndUp">
+              <div
+                class="hover-link scrolled-over d-inline mx-2"
+                @click="goTo(0)"
+              >
+                Home
+              </div>
+              <div
+                v-for="(sec, index) in sections"
+                class="hover-link d-inline mx-2"
+                @click="goTo(sec.ref)"
+                :class="{ 'scrolled-over': scrolledOverState[index] }"
+              >
+                {{ sec?.displayName }}
+              </div>
+            </template>
+            <VBtn
+              v-else
+              @click.stop="drawerVisibility = !drawerVisibility"
+              icon="mdi-menu"
+            ></VBtn>
           </div>
         </VRow>
       </VContainer>
     </VAppBar>
+    <VNavigationDrawer v-model="drawerVisibility" location="right" temporary>
+      <VList class="nav-container">
+        <VListItem class="hover-link scrolled-over" @click="goTo(0)">
+          Home
+        </VListItem>
+        <VListItem
+          v-for="(section, index) in sections"
+          @click="goTo(section.ref)"
+          class="hover-link"
+          :class="{ 'scrolled-over': scrolledOverState[index] }"
+        >
+          {{ section.displayName }}
+        </VListItem>
+      </VList>
+    </VNavigationDrawer>
     <VMain ref="myself">
       <NuxtPage ref="page"></NuxtPage>
     </VMain>
