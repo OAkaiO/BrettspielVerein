5e6dea411ad6c75b35febe5457be7fedd5c51ae3
Lukas Schär <luki.schaer@hispeed.ch>
2025-05-01 19:27:40 +0200

    004: Finalize dev script and update README
    This script centralizes the multiple required scripts

--- buildDist.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DIR=$(pwd)
-
-cd ./dist/app
-composer install --prefer-dist --no-dev --optimize-autoloader
-zip -rD9 ../app.zip . -x ".env*"
-
-cd $pwd
--- db/liquibase/runLiquibaseProd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Run liquibase with the volumes and networks configured, as well as the defaults file
-docker run --rm -v ./changelog:/liquibase/changelog bvz/liquibase-mysql  --defaults-file=/liquibase/changelog/liquibase.prod.properties "$@"
