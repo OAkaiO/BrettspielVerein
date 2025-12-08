3264f46b529ff8f688a55dbd3586eb105435e89c
Lukas Schär <luki.schaer@hispeed.ch>
2025-12-08 14:27:37 +0100

    033: Add alert container

---
+++ vue/app/components/AlertContainer.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const { alertData } = defineProps<{
+  alertData: AlertStatus[];
+}>();
+</script>
+
+<template>
+  <UContainer
+    class="fixed top-30 z-20"
+  >
+    <UAlert
+      v-for="alert in alertData"
+      :key="alert.message"
+      :title="alert.message"
+      :color="alert.type"
+      class="my-4"
+    />
+  </UContainer>
+</template>
