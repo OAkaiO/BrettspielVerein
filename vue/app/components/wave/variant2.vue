44fda84f804e50852a4b12c886720d0ca6267ee5
Lukas Schär <luki.schaer@hispeed.ch>
2025-12-08 18:07:47 +0100

    033: Fix wave variant 2

--- vue/app/components/wave/variant2.vue
+++ vue/app/components/wave/variant2.vue
@@ -16,7 +16,7 @@ const calculatedColor = computed(() => {
       xmlns="http://www.w3.org/2000/svg"
       xmlns:xlink="http://www.w3.org/1999/xlink"
       :class="{ lower: lower }"
-      class="w-100"
+      class="w-full"
       preserveAspectRatio="none"
       style="height: 144px"
     >
