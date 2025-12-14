d9064778b75edc03e511f42a04e195c8d38e3eda
Lukas Schär <luki.schaer@hispeed.ch>
2025-12-14 11:16:09 +0100

    033: Trim all fields when submitting to php

--- vue/app/composables/usePhpBackend.ts
+++ vue/app/composables/usePhpBackend.ts
@@ -3,6 +3,16 @@ export default function<TYPE> (url: string) {
 
   return {
     get: () => $api(url),
-    post: (body: object) => $api(url, { body, method: "POST" }),
+    post: (body: TYPE) => {
+      let t: keyof TYPE;
+      for (t in body) {
+        if (typeof body[t] === "string") {
+          // Because we check the type to be string, the assignment is fine
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          body[t] = (body[t] as string).trim() as any;
+        }
+      }
+      return $api(url, { body: body as object, method: "POST" });
+    },
   };
 }
